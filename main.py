ff6d0854cf51f287e87f8fef07ff58a2e123bbce
thaetim <maciey138@gmail.com>
2021-05-11 17:44:41 +0200

    main file

---
+++ main.py
@@ -0,0 +1,17 @@
+# PROJEKT PROCEDURY BADAWCZEJ
+# wybor procedury, np Stroop, Posner, Go-No-Go, stop-signal itp
+# opis do czego to ma być (po krotce), do czego sluzyc, co badac, jak
+# specyfikacja: dokladny opis procedury, ile triali, jaka kolejnosc, co przez ile trwa wyswietla
+# pisanie procedury miesko projektu glowna czesc
+# test na dwoch trzech osobach - czy zwraca dobre pliki dane i dziala
+# obrona projektu - wylumaczyc jak dla czego dziala, specka i kod, kto za co odpowiadal
+
+# krotki opis i temat: zaakceptowany do piatku (wyslac propo we wt albo sr)
+# 31.05 specyfikacja skonczona
+#   pisanie proceur, testowanie
+
+# obrona projektu do konca czerwca (sami ustalic termin) (trwa 30min okolo)
+# 24h przed obrona - dostarczyc podzial pracy i projekt
+
+if __name__ == "__main__":
+    pass
